ead2d2e11ee371fa6c198d675bc4f167a5f15a4e
happinessozoemena <happinessozoemena9@gmail.com>
2024-02-01 00:44:20 +0100

    update test for Module.3

---
+++ Module.3/Task6.js
@@ -0,0 +1,16 @@
+  // Check if agiven number is odd or even. 
+  //if the number is odd, print to the console:number is odd. if the number is even, print to the console: number is even 
+
+   const numberOfWorkers = 40
+   if (numberOfWorkers %2 ===0) {
+     console.log("numberOfWorkers is even")
+   }else{
+    console.log("numberOfWorkers is odd")
+}
+
+    const numberOfBooks = 23
+    if (numberOfBooks %2 ===0) {
+      console.log("numberOfBooks is even")  
+    }else{
+        console.log("numberOfBooks is odd")
+    }
