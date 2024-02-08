fc34ac0fd8db85a1e744bf693a0b3a25edcd748c
happinessozoemena <happinessozoemena9@gmail.com>
2024-02-08 12:32:53 +0100

    update Module.3

---
+++ Module.3/Task13.js
@@ -0,0 +1,10 @@
+ const book = {
+    title:'the Little Princess',
+    description:'Kindiness',
+    numberOfPages:'250',
+    authour:'Gilbert Smith',
+    reading: true
+
+ }
+
+        console.log(book.title)
