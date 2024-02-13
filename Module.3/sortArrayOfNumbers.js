8ec84e5fa2f4e01c5179874c9b3f793d0beb36c9
happinessozoemena <happinessozoemena9@gmail.com>
2024-02-13 01:34:43 +0100

    update Module.3

--- Module.3/sortArrayOfNumbers.js
+++
@@ -1,7 +0,0 @@
-
-let numbers = [5, 2, 15, 1, 30, 3];
-
-numbers.sort((a, b) => b - a);
-
-console.log(numbers); 
-
--- Module.3/sum.js
+++
@@ -1,5 +0,0 @@
-const numArray = [5, 10, 40, 20];
-
-const sum = numArray.reduce((initial, current)=> initial + current, 0  );
-
-console.log(sum);
