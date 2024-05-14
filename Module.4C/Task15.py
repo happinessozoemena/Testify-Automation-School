c014b5a2c36a7ca1711d2858b965a65a30e96c49
happinessozoemena <150088981+happinessozoemena@users.noreply.github.com>
2024-05-15 00:55:16 +0100

    update test

---
+++ Module.4C/Task15.py
@@ -0,0 +1,17 @@
+#Declare a list with any value and number of item/element
+#Print the list in console
+#Sort the list
+#Print the list in console again
+
+
+# Declare a list with any values and number of items/elements
+my_list = [5, 2, 7, 1, 3, 8, 4, 9]
+
+# Print the original list in the console
+print("Original List:", my_list)
+
+# Sort the list
+my_list.sort()
+
+# Print the sorted list in the console
+print("Sorted List:", my_list)
